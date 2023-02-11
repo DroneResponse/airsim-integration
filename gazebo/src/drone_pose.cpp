f00538eca5005aa8b9a841a3e6c604ef5cd37736
Michael Murphy <murphym18@gmail.com>
2023-02-10 16:06:29 -0800

    WIP split DronePose into sender and reciever

--- gazebo/src/drone_pose.cpp
+++ gazebo/src/drone_pose.cpp
@@ -22,16 +22,7 @@
 #include <gazebo/transport/transport.hh>
 #include <gazebo/msgs/msgs.hh>
 #include <gazebo/gazebo_client.hh>
-#include "common/common_utils/StrictMode.hpp"
-STRICT_MODE_OFF
-#ifndef RPCLIB_MSGPACK
-#define RPCLIB_MSGPACK clmdep_msgpack
-#endif // !RPCLIB_MSGPACK
-#include "rpc/rpc_error.h"
-STRICT_MODE_ON
 
-#include "vehicles/multirotor/api/MultirotorRpcLibClient.hpp"
-#include "Eigen/Dense"
 #include <chrono>
 
 #include <iostream>
@@ -39,12 +30,25 @@ STRICT_MODE_ON
 constexpr int NWIDTH = 7;
 static constexpr int MESSAGE_THROTTLE = 100;
 
-using namespace msr::airlib;
+// we need a struct that has fields for
+// drone position and attitude
+// camera position and attitude
+#pragma pack
+typedef struct {
+    double x;
+    double y;
+    double z;
+    double w;
+    double xi;
+    double yj;
+    double zk;
+} Pose;
 
-msr::airlib::MultirotorRpcLibClient client;
-std::vector<std::string> vehicleList;
-bool rollOverride = false;
-
+#pragma pack
+typedef struct {
+    Pose drone;
+    Pose camera;
+} PoseMessage;
 
 /**
  * local pose callback where gazebo drone represents airsim drone's global pose
@@ -52,17 +56,10 @@ bool rollOverride = false;
 */
 void cbLocalPose(ConstPosesStampedPtr& msg)
 {
+    // "~/pose/local/info" is published at 250 Hz
     std::cout << std::fixed;
     std::cout << std::setprecision(3);
     static int count = 0;
-
-    if (count % MESSAGE_THROTTLE == 0) {
-        std::cout << "The number of vehicles is: " << vehicleList.size();
-        std::cout << "\n";
-
-        std::cout << "The first vehicle is: " << vehicleList[0];
-        std::cout << "\n" << std::endl;
-    }
 
     for (int i = 0; i < msg->pose_size(); i++) {
         auto x = msg->pose(i).position().x();
@@ -85,12 +82,6 @@ void cbLocalPose(ConstPosesStampedPtr& msg)
             std::cout << " oz: " << std::right << std::setw(NWIDTH) << oz;
             std::cout << std::endl;
         }
-        if (i == 0) {
-            msr::airlib::Vector3r p(x, -y, -z);
-            msr::airlib::Quaternionr o(ow, ox, -oy, -oz);
-
-            client.simSetVehiclePose(Pose(p, o), true);
-        }
     }
     if (count % MESSAGE_THROTTLE == 0) {
         std::cout << std::endl;
@@ -98,137 +89,6 @@ void cbLocalPose(ConstPosesStampedPtr& msg)
 
     ++count;
 }
-
-
-/**
- * converts gimbal mounted camera local orientation to a global orientation. 
- * @param camDroneOrientation camera local quaternion where x = roll, y = pitch, z = yaw
- * @param droneOrienation drone global quaternion where x = roll, y = pitch, z = yaw
- * @return camera global quaternion where x = roll, y = pitch, z = yaw
- */
-static msr::airlib::Quaternionr camDroneToGlobal(
-    msr::airlib::Quaternionr camDroneOrientation,
-    msr::airlib::Quaternionr droneOrientation
-) {
-    // need to remove pitch and roll from drone orientation since camera pitch and roll
-    // are already in the global frame
-    float drone_yaw = msr::airlib::VectorMath::getYaw(droneOrientation);
-    msr::airlib::Quaternionr drone_orientation_yaw_only(
-        std::cos(drone_yaw / 2.0),
-        0,
-        0,
-        std::sin(drone_yaw / 2.0));
-
-    return drone_orientation_yaw_only * camDroneOrientation;
-}
-
-
-/**
- * removes roll (y) from the provided quaternion.
- * @param orientation quaternion where x = roll, y = pitch, z = yaw
- * @return quaternion where x = roll, y = pitch, z = yaw
- */
-static msr::airlib::Quaternionr removeRoll(msr::airlib::Quaternionr orientation) {
-    float pitch = msr::airlib::VectorMath::getPitch(orientation);
-    float yaw = msr::airlib::VectorMath::getYaw(orientation);
-
-    msr::airlib::Quaternionr orientation_pitch_only(
-        std::cos(pitch / 2.0),
-        0,
-        std::sin(pitch / 2.0),
-        0);
-
-    msr::airlib::Quaternionr orientation_yaw_only(
-        std::cos(yaw / 2.0),
-        0,
-        0,
-        std::sin(yaw / 2.0));
-
-    // follow assumed previous orientation assembly with roll -> pitch -> yaw
-    return orientation_yaw_only * orientation_pitch_only;
-}
-
-
-/**
- * local pose callback where gazebo typhoon_h480 gimbal camera represents airsim drone's global pose
- * @param msg gazebo message
-*/
-void cbDroneAsCameraPose(ConstPosesStampedPtr& msg)
-{
-    std::cout << std::fixed;
-    std::cout << std::setprecision(3);
-    static int count = 0;
-
-    // initialize with nanf
-    msr::airlib::Quaternionr drone_world_o(std::nanf(""), std::nanf(""), std::nanf(""), std::nanf(""));
-    msr::airlib::Quaternionr cam_drone_o(std::nanf(""), std::nanf(""), std::nanf(""), std::nanf(""));
-    msr::airlib::Quaternionr cam_global_o(std::nanf(""), std::nanf(""), std::nanf(""), std::nanf(""));
-    msr::airlib::Vector3r p(std::nanf(""), std::nanf(""), std::nanf(""));
-    
-    if (count % MESSAGE_THROTTLE == 0) {
-        std::cout << "The number of vehicles is: " << vehicleList.size();
-        std::cout << "\n";
-
-        std::cout << "The first vehicle is: " << vehicleList[0];
-        std::cout << "\n" << std::endl;
-    }
-
-    for (int i = 0; i < msg->pose_size(); i++) {
-        auto x = msg->pose(i).position().x();
-        auto y = msg->pose(i).position().y();
-        auto z = msg->pose(i).position().z();
-        auto ow = msg->pose(i).orientation().w();
-        auto ox = msg->pose(i).orientation().x();
-        auto oy = msg->pose(i).orientation().y();
-        auto oz = msg->pose(i).orientation().z();
-        if (count % MESSAGE_THROTTLE == 0) {
-            std::cout << "local (" << std::setw(2) << i << ") ";
-            std::cout << std::left << std::setw(32) << msg->pose(i).name();
-            std::cout << " x: " << std::right << std::setw(NWIDTH) << x;
-            std::cout << " y: " << std::right << std::setw(NWIDTH) << y;
-            std::cout << " z: " << std::right << std::setw(NWIDTH) << z;
-
-            std::cout << " ow: " << std::right << std::setw(NWIDTH) << ow;
-            std::cout << " ox: " << std::right << std::setw(NWIDTH) << ox;
-            std::cout << " oy: " << std::right << std::setw(NWIDTH) << oy;
-            std::cout << " oz: " << std::right << std::setw(NWIDTH) << oz;
-            std::cout << std::endl;
-        }
-        // update freq ~250 hz
-        if (i == 0) {
-            // drone position and orientaiton in global frame
-            p = msr::airlib::Vector3r(x, -y, -z);
-            drone_world_o = msr::airlib::Quaternionr(ow, ox, -oy, -oz);
-            if (count % MESSAGE_THROTTLE == 0) {
-                std::cout << "Camera drone quaternion (xyzw): \n" << cam_drone_o.coeffs() << std::endl;
-                std::cout << "Drone world quaternion (xyzw): \n" << drone_world_o.coeffs() << std::endl;
-            }
-        }
-        // set drone attitude from camera attitude
-        if (msg->pose(i).name() == "typhoon_h480::cgo3_camera_link") {
-            // orientation of the camera in drone's reference frame
-            if (rollOverride) {
-                cam_drone_o = removeRoll(msr::airlib::Quaternionr(ow, ox, -oy, -oz));
-            } else {
-                cam_drone_o = msr::airlib::Quaternionr(ow, ox, -oy, -oz);
-            }
-            if (count % MESSAGE_THROTTLE == 0) {
-                std::cout << "Camera drone quaternion (xyzw): \n" << cam_drone_o.coeffs() << std::endl;
-                std::cout << "Drone world quaternion (xyzw): \n" << drone_world_o.coeffs() << std::endl;
-            }
-        }
-
-        cam_global_o = camDroneToGlobal(cam_drone_o, drone_world_o);
-        // TODO: loop through vehicles for multidrone sim
-        client.simSetVehiclePose(Pose(p, cam_global_o), true, vehicleList[0]);
-    }
-    if (count % MESSAGE_THROTTLE == 0) {
-        std::cout << std::endl;
-    }
-
-    ++count;
-}
-
 
 /**
  * global pose callback that simply prints global poses
@@ -261,31 +121,22 @@ void cbGlobalPose(ConstPosesStampedPtr& msg)
     std::cout << std::endl;
 }
 
+
+send_pose_message(const PoseMessage& pose_message, socket_t& socket)
+{
+    // send pose message to airsim
+    zmq::message_t message(sizeof(PoseMessage));
+    memcpy(message.data(), &pose_message, sizeof(PoseMessage));
+    socket.send(message);
+
+}
+
 int main(int argc, char** argv)
 {
-    void (*localPoseCallback)(ConstPosesStampedPtr&) = &cbLocalPose;
-    for (int i=0; i < argc; i++) {
-        if (strcmp(argv[i], "-c") == 0) {
-            localPoseCallback = &cbDroneAsCameraPose;
-        }
-        if (strcmp(argv[i], "-r") == 0) {
-            rollOverride = true;
-        }
-    }
-
-    if (localPoseCallback == &cbDroneAsCameraPose) {
-        std::cout << "Drone pose will be set to global gimbal camera pose\n";
-    } else {
-        std::cout << "Drone pose will be set to global drone pose\n";
-    }
-
-    if (rollOverride) {
-        std::cout << "Camera roll will be artifically removed\n";
-    }
-
-    client.confirmConnection();
-    // don't want to call on every message because blocks for too long
-    vehicleList = client.listVehicles();
+    std::string air_sim_host = "127.0.0.1";
+    int air_sim_port = 41451;
+    // print out the version of gazebo
+    std::cout << "Gazebo version: " << GAZEBO_MAJOR_VERSION << "." << GAZEBO_MINOR_VERSION << std::endl;
 
     // Load gazebo
     gazebo::client::setup(argc, argv);
@@ -296,7 +147,7 @@ int main(int argc, char** argv)
 
     // Listen to Gazebo topics
     // update freq ~250 hz
-    gazebo::transport::SubscriberPtr sub_pose1 = node->Subscribe("~/pose/local/info", *localPoseCallback);
+    gazebo::transport::SubscriberPtr sub_pose1 = node->Subscribe("~/pose/local/info", cbLocalPose);
     // update freq ~50 hz
     gazebo::transport::SubscriberPtr sub_pose2 = node->Subscribe("~/pose/info", cbGlobalPose);
 
