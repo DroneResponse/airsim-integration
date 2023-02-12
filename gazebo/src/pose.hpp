ad4c56b3a27a98cfa63c2da67285fed5fd278416
Michael Murphy <murphym18@gmail.com>
2023-02-12 11:52:16 -0800

    Fix alignment of structs plus do more to pose.hpp
    Fix the "#pragma pack" directives. The directive needed a byte alignment
    value. In our case we want to align on 1 byte boundaries. We also want
    to restore the original byte alignment setting. So I used the push and
    pop feature of #pragma pack. The GCC docs for #pragma pack are here:
    
    https://gcc.gnu.org/onlinedocs/gcc-4.9.3/gcc/Structure-Packing-Pragmas.html#Structure-Packing-Pragmas
    
    Add a message_coutner field to PoseMessage. This will allow us to reject
    old messages that arrive out of order.
    
    Finally, clean up pose.hpp. Use the C/C++ idiom to define a struct with
    a typedef. This prevents an object, function, or enumerator from hiding
    the struct name. The problem with the old code is subtle and it's
    described well by the following stackoverflow answers:
    
        This describes what's going on:
        https://stackoverflow.com/a/1675446
    
        This describes why it matters:
        https://stackoverflow.com/a/612476
    
    See GitHub issue: #14
    
    Co-authored-by: Jason Brauer <JasonBrauer@users.noreply.github.com>

--- gazebo/src/pose.hpp
+++ gazebo/src/pose.hpp
@@ -1,11 +1,13 @@
 #ifndef POSE_H
 #define POSE_H
 
+#include <stdint.h>
+
 // we need a struct that has fields for
 // drone position and attitude
 // camera position and attitude
-#pragma pack
-typedef struct {
+#pragma pack(push,1)
+typedef struct Pose {
     double x;
     double y;
     double z;
@@ -15,10 +17,12 @@ typedef struct {
     double zk;
 } Pose;
 
-#pragma pack
-typedef struct {
+typedef struct PoseMessage {
+    uint64_t message_counter; // this is a counter that increments every time we send a message
     Pose drone;
     Pose camera;
 } PoseMessage;
+
+#pragma pack(pop)
 
 #endif
