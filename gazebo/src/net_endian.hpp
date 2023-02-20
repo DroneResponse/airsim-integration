554be3d7dca4eee32dc77f1417a1bd4592dbc3e0
JasonBrauer <jbrauer90@gmail.com>
2023-02-20 14:50:35 -0700

    rm constexpr from net_endian functions

--- gazebo/src/net_endian.hpp
+++ gazebo/src/net_endian.hpp
@@ -4,14 +4,14 @@
 
 namespace net_bits {
 
-    inline constexpr int64_t reverse_bytes(const int64_t value) {
+    inline int64_t reverse_bytes(const int64_t value) {
         // if we have 0x 22222222 11111111
         auto hb = (int64_t) htonl(value) << 32; // 11111111 00000000
         auto lb = (int64_t) htonl(value >> 32); // 00000000 22222222
         return hb + lb; // 11111111 22222222
     }
 
-    inline constexpr int64_t hton64(const int64_t value) {
+    inline int64_t hton64(const int64_t value) {
         // This is a "constexpr if statement" If native == little, then the compiler
         // will discarded the false branch otherwise the true branch is discarded.
         if constexpr (std::endian::native == std::endian::little) {
@@ -22,19 +22,19 @@ namespace net_bits {
         }
     }
 
-    inline constexpr int64_t ntoh64(const int64_t value) {
+    inline int64_t ntoh64(const int64_t value) {
         return hton64(value);
     }
 
     // todo: make this a template
-    inline constexpr uint64_t reverse_bytes(const uint64_t value) {
+    inline uint64_t reverse_bytes(const uint64_t value) {
         // if we have 0x 22222222 11111111
         auto hb = (uint64_t) htonl(value) << 32; // 11111111 00000000
         auto lb = (uint64_t) htonl(value >> 32); // 00000000 22222222
         return hb + lb; // 11111111 22222222
     }
 
-    inline constexpr uint64_t hton64(const uint64_t value) {
+    inline uint64_t hton64(const uint64_t value) {
         // This is a "constexpr if statement" If native == little, then the compiler
         // will discarded the false branch otherwise the true branch is discarded.
         if constexpr (std::endian::native == std::endian::little) {
@@ -45,7 +45,7 @@ namespace net_bits {
         }
     }
 
-    inline constexpr uint64_t ntoh64(const uint64_t value) {
+    inline uint64_t ntoh64(const uint64_t value) {
         return hton64(value);
     }
 
