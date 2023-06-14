9ebde21900bdf98effa70a63c1cf6707bbeb0897
JasonBrauer <jbrauer90@gmail.com>
2023-06-14 15:22:17 -0600

    constructor for MockUDPSender

--- pose/test/test_udp_sender.hpp
+++ pose/test/test_udp_sender.hpp
@@ -5,5 +5,6 @@
 
 class MockUDPSender : public UDPSender {
     public:
-        MOCK_METHOD(void, send_pose_message, (PoseTransfer::PoseMessage));
+        MockUDPSender() : UDPSender("1.1.1.1", 5555) {};
+        MOCK_METHOD(void, send_pose_message, (PoseTransfer::PoseMessage pose_message));
 };
