b5d81a708ba0d3cb8c75b0d1f4aaad5613254615
JasonBrauer <jbrauer90@gmail.com>
2023-06-16 16:39:57 -0600

    trying to validate send_pose_message called with PoseMessage containing expected values

--- pose/gazebo/test/test_gazebo_drone_pose.cpp
+++ pose/gazebo/test/test_gazebo_drone_pose.cpp
@@ -3,10 +3,14 @@
 #include <gazebo/msgs/msgs.hh>
 
 #include "gazebo_drone_pose.hpp"
+#include "pose.hpp"
 #include "test_udp_sender.hpp"
 
-https://google.github.io/googletest/gmock_for_dummies.html#using-mocks-in-tests
+// https://google.github.io/googletest/gmock_for_dummies.html#using-mocks-in-tests
 using ::testing::AtLeast;
+using ::testing::Eq;
+using ::testing::SaveArg;
+using ::testing::DoAll;
 
 
 /**
@@ -14,40 +18,80 @@ using ::testing::AtLeast;
  * UDPSender pose message
 */
 TEST(TestDronePose, TestMultipleDronesSendPoseMessages) {
+    PoseTransfer::Pose mock_drone_0_pose {
+        .x = 1.1,
+        .y = 2.2,
+        .z = 3.3,
+        .w = 0.7073883,
+        .xi = 0.0,
+        .yj = 0.7068252,
+        .zk = 0.0
+    };
+    PoseTransfer::Pose mock_camera_0_pose;
+    memset(&mock_camera_0_pose, 0, sizeof(mock_camera_0_pose));
+    PoseTransfer::PoseMessage mock_pose_message_0 {
+        .message_counter = 0,
+        .drone = mock_drone_0_pose,
+        .camera = mock_camera_0_pose
+    };
+    PoseTransfer::Pose mock_drone_1_pose {
+        .x = 11.11,
+        .y = 22.22,
+        .z = 33.33,
+        .w = 0.0007963,
+        .xi = 0.9999997,
+        .yj = 0.0,
+        .zk = 0.0
+    };
+    PoseTransfer::Pose mock_camera_1_pose;
+    memset(&mock_camera_1_pose, 0, sizeof(mock_camera_1_pose));
+    PoseTransfer::PoseMessage mock_pose_message_1 {
+        .message_counter = 1,
+        .drone = mock_drone_1_pose,
+        .camera = mock_camera_1_pose
+    };
+    
+
     gazebo::msgs::PosesStamped mockMsg;
     
     // set drone_0 pose
     gazebo::msgs::Pose* pose0 = mockMsg.add_pose();
     gazebo::msgs::Vector3d* position0 = pose0->mutable_position();
-    position0->set_x(1.1);
-    position0->set_y(2.2);
-    position0->set_z(3.3);
+    position0->set_x(mock_drone_0_pose.x);
+    position0->set_y(mock_drone_0_pose.y);
+    position0->set_z(mock_drone_0_pose.z);
 
     gazebo::msgs::Quaternion* quaternion0 = pose0->mutable_orientation();
-    quaternion0->set_w(0.7073883);
-    quaternion0->set_x(0.0);
-    quaternion0->set_y(0.7068252);
-    quaternion0->set_z(0.0);
+    quaternion0->set_w(mock_drone_0_pose.w);
+    quaternion0->set_x(mock_drone_0_pose.xi);
+    quaternion0->set_y(mock_drone_0_pose.yj);
+    quaternion0->set_z(mock_drone_0_pose.zk);
 
     pose0->set_name("drone_0");
 
     // set drone_1 pose
     gazebo::msgs::Pose* pose1 = mockMsg.add_pose();;
     gazebo::msgs::Vector3d* position1 = pose1->mutable_position();
-    position1->set_x(11.11);
-    position1->set_y(22.22);
-    position1->set_z(33.33);
+    position1->set_x(mock_drone_1_pose.x);
+    position1->set_y(mock_drone_1_pose.y);
+    position1->set_z(mock_drone_1_pose.z);
 
     gazebo::msgs::Quaternion* quaternion1 = pose1->mutable_orientation();
-    quaternion1->set_w(0.0007963);
-    quaternion1->set_x(0.9999997);
-    quaternion1->set_y(0.0);
-    quaternion1->set_z(0.0);
+    quaternion1->set_w(mock_drone_1_pose.w);
+    quaternion1->set_x(mock_drone_1_pose.xi);
+    quaternion1->set_y(mock_drone_1_pose.yj);
+    quaternion1->set_z(mock_drone_1_pose.zk);
 
     pose1->set_name("drone_1");
 
     MockUDPSender mockUdpSender;
     EXPECT_CALL(mockUdpSender, create_socket()).Times(1);
+    PoseTransfer::PoseMessage actual_pose_message;
+    // trying to validate send_pose_message called with PoseMessage containing expected values
+    // https://google.github.io/googletest/gmock_cook_book.html#SaveArgVerify
+    // EXPECT_CALL(mockUdpSender, send_pose_message)
+    //     .WillOnce(DoAll(SaveArg<1>(&actual_pose_message)));
+    // EXPECT_THAT(actual_pose_message.)
 
     GenerateCbLocalPose generateCbLocalPose (&mockUdpSender);
 
