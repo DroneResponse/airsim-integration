84b6b5f44a322543dd13d5b4d048a327dc2cdf18
JasonBrauer <jbrauer90@gmail.com>
2023-06-16 15:23:51 -0600

    update the way data is assigned in the mock pose variables within TestMultipleDronesSendPoseMessages

--- pose/gazebo/test/test_gazebo_drone_pose.cpp
+++ pose/gazebo/test/test_gazebo_drone_pose.cpp
@@ -5,7 +5,7 @@
 #include "gazebo_drone_pose.hpp"
 #include "test_udp_sender.hpp"
 
-
+https://google.github.io/googletest/gmock_for_dummies.html#using-mocks-in-tests
 using ::testing::AtLeast;
 
 
@@ -18,52 +18,43 @@ TEST(TestDronePose, TestMultipleDronesSendPoseMessages) {
     
     // set drone_0 pose
     gazebo::msgs::Pose* pose0 = mockMsg.add_pose();
-    gazebo::msgs::Vector3d position0;
-    position0.set_x(1.1);
-    position0.set_y(2.2);
-    position0.set_z(3.3);
+    gazebo::msgs::Vector3d* position0 = pose0->mutable_position();
+    position0->set_x(1.1);
+    position0->set_y(2.2);
+    position0->set_z(3.3);
 
-    pose0->set_allocated_position(&position0);
-
-    gazebo::msgs::Quaternion quaternion0;
-    quaternion0.set_w(0.7073883);
-    quaternion0.set_x(0.0);
-    quaternion0.set_y(0.7068252);
-    quaternion0.set_z(0.0);
-
-    pose0->set_allocated_orientation(&quaternion0);
+    gazebo::msgs::Quaternion* quaternion0 = pose0->mutable_orientation();
+    quaternion0->set_w(0.7073883);
+    quaternion0->set_x(0.0);
+    quaternion0->set_y(0.7068252);
+    quaternion0->set_z(0.0);
 
     pose0->set_name("drone_0");
 
     // set drone_1 pose
     gazebo::msgs::Pose* pose1 = mockMsg.add_pose();;
-    gazebo::msgs::Vector3d position1;
-    position1.set_x(11.11);
-    position1.set_y(22.22);
-    position1.set_z(33.33);
+    gazebo::msgs::Vector3d* position1 = pose1->mutable_position();
+    position1->set_x(11.11);
+    position1->set_y(22.22);
+    position1->set_z(33.33);
 
-    pose1->set_allocated_position(&position1);
-
-    gazebo::msgs::Quaternion quaternion1;
-    quaternion1.set_w(0.0007963);
-    quaternion1.set_x(0.9999997);
-    quaternion1.set_y(0.0);
-    quaternion1.set_z(0.0);
-
-    pose1->set_allocated_orientation(&quaternion1);
+    gazebo::msgs::Quaternion* quaternion1 = pose1->mutable_orientation();
+    quaternion1->set_w(0.0007963);
+    quaternion1->set_x(0.9999997);
+    quaternion1->set_y(0.0);
+    quaternion1->set_z(0.0);
 
     pose1->set_name("drone_1");
 
-    // somewhere in test above ^
-    // test_gazebo_drone_pose(2712,0x202374600) malloc: *** error for object 0x308454128: pointer being freed was not allocated
-    // test_gazebo_drone_pose(2712,0x202374600) malloc: *** set a breakpoint in malloc_error_break to debug
-    // lldb: https://lldb.llvm.org/use/tutorial.html
-
     MockUDPSender mockUdpSender;
+    EXPECT_CALL(mockUdpSender, create_socket()).Times(1);
+
     GenerateCbLocalPose generateCbLocalPose (&mockUdpSender);
-    // https://www.boost.org/doc/libs/1_36_0/libs/smart_ptr/shared_ptr.htm constructor information
-    ConstPosesStampedPtr mockConstMsg (&mockMsg);
-    generateCbLocalPose.cbLocalPose(mockConstMsg);
+
+    https://www.boost.org/doc/libs/1_55_0/libs/smart_ptr/shared_ptr.htm
+    ConstPosesStampedPtr mockConstMsgPtr ( new const gazebo::msgs::PosesStamped(mockMsg) );
+    generateCbLocalPose.cbLocalPose(mockConstMsgPtr);
+    
 }
 
 
--- pose/test/test_udp_sender.hpp
+++ pose/test/test_udp_sender.hpp
@@ -7,4 +7,5 @@ class MockUDPSender : public UDPSender {
     public:
         MockUDPSender() : UDPSender("1.1.1.1", 5555) {};
         MOCK_METHOD(void, send_pose_message, (PoseTransfer::PoseMessage pose_message));
+        MOCK_METHOD(void, create_socket, ());
 };
