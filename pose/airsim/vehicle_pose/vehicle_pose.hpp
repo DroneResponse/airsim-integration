e4ba6254163e76075e6b4ef98faefa5aa5c36f6e
JasonBrauer <jbrauer90@gmail.com>
2023-06-22 16:59:28 -0600

    define first version of VehiclePose abstract class

---
+++ pose/airsim/vehicle_pose/vehicle_pose.hpp
@@ -0,0 +1,31 @@
+#include <string>
+
+#include "pose.h"
+
+#ifndef VEHICLE_POSE_H
+#define VEHICLE_POSE_H
+
+namespace SimulatorInterface {
+    class VehiclePose {
+        public:
+            /** Pass a pointer to the relevant simulation client. Simulator specific client details are
+             * to be implemented within the public functions of this class
+             * @param sim_client pointer to a simulation client
+            */
+            VehiclePose(void *sim_client) {};
+            virtual ~VehiclePose() {};
+            /** Spawns a new vehicle with the provided unique id
+             * @param vehicle_id unique vehicle id within a simulation instance
+            */
+            virtual spawn_vehicle(std::string vehicle_id)
+            /** updates the vehicle pose via a simulator client for a given unique vehicle id
+             * @param pose pose to be set
+             * @param vehicle_id unique id of a vehicle within the simulation
+            */
+            virtual set_vehicle_pose(PoseTransfer::Pose pose, std::string vehicle_id) {};
+        private:
+            void *sim_client
+    };
+}
+
+#endif
